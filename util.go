e83050fadc538c1cd3ae5b727b737044a9793ad8
Guide Fari <guideg6@gmail.com>
2024-05-14 05:20:35 +0200

    clean up utils

--- util.go
+++ util.go
@@ -1,9 +1,11 @@
 package main
 
-import "io/ioutil"
+import (
+	"os"
+)
 
 func readFile(fname string) string {
-	databyte, err := ioutil.ReadFile(fname)
+	databyte, err := os.ReadFile(fname)
 	checkErr(err)
 	return string(databyte)
 }
@@ -12,5 +14,18 @@ func checkErr(err error) {
 	if err != nil {
 		panic(err)
 	}
+
+}
+
+func openOrCreateFile(name string) (*os.File, error) {
+
+	file, err := os.OpenFile(name, os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil && err.Error() == "open "+name+": no such file or directory" {
+		file, _ = os.Create(name)
+		local_log.Printf("File Created: " + name)
+		return file, nil
+	}
+
+	return file, nil
 
 }
