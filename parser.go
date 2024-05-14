64ec9e3750577323584ed2d648a781da710e69fd
Guide Fari <guideg6@gmail.com>
2024-05-14 04:37:33 +0200

    more structure for parser file

--- parser.go
+++ parser.go
@@ -16,4 +16,18 @@ func ParseRss() {
 	feedList := [2]string{"https://goosebumps.fm/rss.xml", "http://musicforprogramming.net/rss.php"}
 	feedParser := gofeed.NewParser()
 	feedParser.Client = &http.Client{Timeout: time.Second * 10}
+
+	feed_items := make([]FeedItem, 1)
+
+	for true {
+		for k := 0; k < len(feedList); k++ {
+
+		}
+
+		// Regenrate the items list
+		feed_items = make([]FeedItem, 1)
+
+		// Wait 8 hours
+		time.Sleep(28800 * time.Second)
+	}
 }
